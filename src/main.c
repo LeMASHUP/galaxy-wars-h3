19c34432816cb4ea3dc9c2f4756da5a3b5595b3f
LeMASHUP <dutertre.zakaria@gmail.com>
2022-11-14 14:34:51 +0100

    Début developpement
    Affichage d'une fenêtre blanche et création d'un état de jeu

--- src/main.c
+++ src/main.c
@@ -6,7 +6,17 @@
 # pragma comment(lib, "h3-s.lib")
 #endif // !NDEBUG
 
+int state;
+state = 1;
+
 int main(int argc, char** argv)
 {
+	while (state = 1) {
+		H3Handle screen_cam = H3_Init((SH3InitParams) {
+			.fullscreen = false,
+				.height = 500,
+				.width = 500,
+		});
+	};
 	return 0;
 }
