f39dd7923c41cbaaa649c9db6094643eac368ea2
LeMASHUP <dutertre.zakaria@gmail.com>
2022-11-14 15:37:33 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,8 +12,9 @@ int main(int argc, char** argv)
 
 		H3Handle h3 = H3_Init((SH3InitParams) {
 			.fullscreen = false,
-				.height = 500,
-				.width = 500,
+			.height = 500,
+			.width = 500,
+			.windowTitle = "Galaxy Wars H3 Engine"
 		});
 	H3Handle scene = H3_Scene_Create(h3, false);
 	while (h3, scene);
