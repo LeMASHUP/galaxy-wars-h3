798e40ced2e45c920fdc1fb90d29e9b1838e51d2
LeMASHUP <dutertre.zakaria@gmail.com>
2022-11-14 15:29:37 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,14 +9,13 @@
 
 int main(int argc, char** argv)
 {
-	H3Handle scene1 = H3_Scene_Create(H3Handle h3, bool physicsLocksRotation);
 
 		H3Handle h3 = H3_Init((SH3InitParams) {
 			.fullscreen = false,
 				.height = 500,
 				.width = 500,
 		});
-
-	while (H3Handle h3, scene1);
+	H3Handle scene = H3_Scene_Create(h3, true);
+	while (h3, scene);
 	return 0;
 }
