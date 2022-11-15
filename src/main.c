57c4b008a48c4a5365aab3b5e0537a8295f39092
LeMASHUP <dutertre.zakaria@gmail.com>
2022-11-15 14:39:30 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,14 +13,19 @@ int main(int argc, char** argv)
 
 		H3Handle h3 = H3_Init((SH3InitParams) {
 			.fullscreen = false,
-			.height = 500,
-			.width = 500,
+			.height = 800,
+			.width = 800,
 			.windowTitle = "Galaxy Wars H3 Engine"
 		});
 	H3Handle scene = H3_Scene_Create(h3, false);
 	H3Handle player = H3_Object_Create(scene,"player", NULL);
 
 	H3_Object_AddComponent(player, SPRITECOMPONENT_CREATE("assets/Player.png", 0x22));
+	H3_Object_SetScale(player, 0.9);
+	H3_Object_GetEnabled(player);
+	H3_Object_SetEnabled(player, true);
+	
+
 
 	while (h3, scene);
 	return 0;
