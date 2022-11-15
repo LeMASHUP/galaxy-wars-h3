89e8a635d59bb371e0ac65cf7ac95fb0bec899a1
LeMASHUP <dutertre.zakaria@gmail.com>
2022-11-15 15:48:50 +0100

    Update main.c
    Ajout d'un int width et height pour simplifier à l'avenir

--- src/main.c
+++ src/main.c
@@ -7,14 +7,18 @@
 # pragma comment(lib, "h3-s.lib")
 #endif // !NDEBUG
 
+int width;
+int height;
 
 int main(int argc, char** argv)
 {
+	width = 500;
+	height = 700;
 
 	H3Handle h3 = H3_Init((SH3InitParams) {
 		.fullscreen = false,
-			.height = 500,
-			.width = 500,
+			.height = height,
+			.width = width,
 			.windowTitle = "Galaxy Wars H3 Engine"
 	});
 	H3Handle scene = H3_Scene_Create(h3, false);
@@ -22,6 +26,7 @@ int main(int argc, char** argv)
 
 	H3_Object_AddComponent(player, SPRITECOMPONENT_CREATE("assets/Player.png", 0x22));
 	H3_Object_SetRenderOrder(player, 1);
+	H3_SetView2(h3, 0, 0, width, height);
 
 
 	while (H3_DoFrame(h3, scene)) {
