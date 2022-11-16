31e6f4857ad5fbdd47c0928ca99ac6e36ebfa95a
LeMASHUP <dutertre.zakaria@gmail.com>
2022-11-16 14:31:59 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,24 +9,30 @@
 
 int width;
 int height;
+int x;
+int y;
 
 int main(int argc, char** argv)
 {
 	width = 500;
 	height = 700;
+	
 
 	H3Handle h3 = H3_Init((SH3InitParams) {
 		.fullscreen = false,
-			.height = height,
-			.width = width,
-			.windowTitle = "Galaxy Wars H3 Engine"
+		.height = height,
+		.width = width,
+		.windowTitle = "Galaxy Wars Hydrogen Engine 3"
 	});
+
+
 	H3Handle scene = H3_Scene_Create(h3, false);
 	H3Handle player = H3_Object_Create(scene, "player", NULL);
 
 	H3_Object_AddComponent(player, SPRITECOMPONENT_CREATE("assets/Player.png", 0x22));
 	H3_Object_SetRenderOrder(player, 1);
 	H3_SetView2(h3, 0, 0, width, height);
+	
 
 
 	while (H3_DoFrame(h3, scene)) {
