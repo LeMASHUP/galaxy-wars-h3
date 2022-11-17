7b786b8e7446d679659d69cbf1c5e7dd0e0fa1a4
LeMASHUP <dutertre.zakaria@gmail.com>
2022-11-17 10:24:25 +0100

    Ajout du project dir

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <h3.h>
 #include "components\spritecomponent.h"
+#include "components\cameracomponent.h"
+#include <stdlib.h>
 
 #ifndef NDEBUG
 # pragma comment(lib, "h3-s-d.lib")
@@ -9,13 +11,15 @@
 
 int width;
 int height;
-int x;
-int y;
+int mousex;
+int mousey;
+float angle;
 
 int main(int argc, char** argv)
 {
-	width = 500;
-	height = 700;
+	width = 1280;
+	height = 720;
+	angle = 50;
 	
 
 	H3Handle h3 = H3_Init((SH3InitParams) {
@@ -26,16 +30,19 @@ int main(int argc, char** argv)
 	});
 
 
-	H3Handle scene = H3_Scene_Create(h3, false);
+	H3Handle scene = H3_Scene_Create(h3, true);
 	H3Handle player = H3_Object_Create(scene, "player", NULL);
 
 	H3_Object_AddComponent(player, SPRITECOMPONENT_CREATE("assets/Player.png", 0x22));
 	H3_Object_SetRenderOrder(player, 1);
 	H3_SetView2(h3, 0, 0, width, height);
+	H3_Object_Translate(player, 0, 0);
 	
 
 
 	while (H3_DoFrame(h3, scene)) {
+		H3_Input_GetMousePos(h3, &mousex, &mousey);
+		H3_Object_Rotate(player, angle);
 
 
 	}
