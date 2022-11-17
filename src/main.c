98a8a8e66c8e33d0eb623d2daf57682a82a48560
LeMASHUP <dutertre.zakaria@gmail.com>
2022-11-17 13:38:14 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "components\spritecomponent.h"
 #include "components\cameracomponent.h"
 #include <stdlib.h>
+#include<math.h>
 
 #ifndef NDEBUG
 # pragma comment(lib, "h3-s-d.lib")
@@ -14,12 +15,14 @@ int height;
 int mousex;
 int mousey;
 float angle;
+float playerx;
+float playery;
 
 int main(int argc, char** argv)
 {
 	width = 1280;
 	height = 720;
-	angle = 25;
+	angle = atan2(mousey - playery, mousex - playerx) * 180 / 3.141593;
 	
 
 	H3Handle h3 = H3_Init((SH3InitParams) {
@@ -37,12 +40,13 @@ int main(int argc, char** argv)
 	H3_Object_SetRenderOrder(player, 1);
 	H3_SetView2(h3, 0, 0, width, height);
 	H3_Object_Translate(player, 0, 0);
+	H3_Transform_GetPosition(H3_Object_GetTransform(player), &playerx, &playery);
 	
 
 
 	while (H3_DoFrame(h3, scene)) {
 		H3_Input_GetMousePos(h3, &mousex, &mousey);
-		H3_Object_Rotate(player, angle);
+		H3_Object_SetRotation(player, angle);
 
 
 	}
